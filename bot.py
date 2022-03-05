006a00ccc103d82b14c3049c999b38c0fb8f269c
asdkmm5050 <asdkmm505@gmail.com>
2022-03-05 16:50:59 +0800

    change to slash command bot

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
-from tqdm import asyncio
+from discord_slash import SlashCommand, cog_ext
+
 from Audio import Audio
 import discord
 
@@ -10,7 +11,7 @@ class VoiceBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @cog_ext.cog_slash(name='join')
     async def join(self, ctx):
         """Joins a voice channel"""
 
@@ -18,30 +19,34 @@ class VoiceBot(commands.Cog):
             return await ctx.voice_client.move_to(ctx.author.voice.channel)
 
         await ctx.author.voice.channel.connect()
+        return await ctx.send("哈哈，4我啦。", delete_after=1.)
 
-    @commands.command()
+    @cog_ext.cog_slash(name='leave')
     async def leave(self, ctx):
         """leaves a voice channel"""
 
         await ctx.voice_client.disconnect()
+        return await ctx.send("債眷。", delete_after=1.)
 
-    @commands.command()
-    async def play(self, ctx, arg):
+    @cog_ext.cog_slash(name='talk')
+    async def talk(self, ctx, arg):
         """plays the audio from ctx"""
 
         if ctx.voice_client is None:
-            return await ctx.send("沒進頻道歐")
+            return await ctx.send('沒進頻道歐')
         if arg is None:
-            return await ctx.send("我只會讀稿不會讀心歐")
+            return await ctx.send('我只會讀稿不會讀心歐')
         if ctx.voice_client.is_playing():
-            return await ctx.send("我只有一張嘴吧，你們要自立自強阿")
+            return await ctx.send('我只有一張嘴吧，你們要自立自強阿')
         Audio(arg)
         ctx.voice_client.play(discord.FFmpegPCMAudio(executable='C:/ffmpeg/bin/ffmpeg.exe', source='output.mp3'))
+        return await ctx.send('已送出', delete_after=1.)
 
 
 def main():
     bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                       description='Relatively simple music bot example')
+                       description='py')
+    slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
 
     @bot.event
     async def on_ready():

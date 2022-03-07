7a60c4509dbdcabfbeaff5fc6a062e55f9cdd247
asdkmm5050 <asdkmm505@gmail.com>
2022-03-07 15:09:43 +0800

    add language parameter

--- Audio.py
+++ Audio.py
@@ -3,11 +3,11 @@ from gtts import gTTS
 
 
 class Audio:
-    def __init__(self, text, channel):
+    def __init__(self, text, channel, language):
         super(Audio, self).__init__()
         self.text = text
         self.language = 'zh-TW'
-        self.output = gTTS(text=self.text, lang='zh', slow=False)
+        self.output = gTTS(text=self.text, lang=language, slow=False)
         self.saveAudio(channel)
 
     def saveAudio(self, channel):
